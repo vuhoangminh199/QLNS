1000ee3d6285f5df93c89b0b92cf9c55ae52d0ca
KYOUSUKE\Kyousuke <shockwave71288@gmail.com>
2017-09-12 14:55:57 +0700

    add layput sign in - sign up

--- QLNSApp/app/src/main/java/com/tma/hoangminh/qlnsapp/Presentation/Activities/MainActivity.java
+++ QLNSApp/app/src/main/java/com/tma/hoangminh/qlnsapp/Presentation/Activities/MainActivity.java
@@ -5,6 +5,7 @@ package com.tma.hoangminh.qlnsapp.Presentation.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 import com.tma.hoangminh.qlnsapp.R;
 
@@ -14,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TabHost tab = (TabHost) findViewById(android.R.id.tabhost);
+        tab.setup();
+        TabHost.TabSpec spec;
+
+        //tab Dang Nhap
+        spec = tab.newTabSpec("t1");
+        spec.setContent(R.id.tabDangnhap);
+        spec.setIndicator("Đăng Nhập");
+        tab.addTab(spec);
+
+        //tab Dang Ky
+        spec = tab.newTabSpec("t2");
+        spec.setContent(R.id.tabDangky);
+        spec.setIndicator("Đăng Ký");
+        tab.addTab(spec);
+
+        //set default tab
+        tab.setCurrentTab(0);
     }
 }
